5bd846fbe3c8e1214c3c96ad74bc11d21d31b9ee
Chowdhury <archowdhury@deloitte.com>
2020-11-15 09:45:11 -0800

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+//const profileDataArgs = process.argv.slice(2, process.argv.length);
 // const name = profileDataArgs[0];
 // const github = profileDataArgs[1];
-const [name, github] = profileDataArgs;
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+// const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 // console.log(profileDataArgs);
 // const printProfileData = profileDataArr => {
 //     // This...
@@ -22,8 +22,29 @@ const generatePage = require('./src/page-template.js');
 
 // console.log(name, github);
 // console.log(generatePage(name, github));
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+//console.log(inquirer);
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
