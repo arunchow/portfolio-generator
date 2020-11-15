7bb44d7c814b79834e69ef855be0a2235c1f68c2
Chowdhury <archowdhury@deloitte.com>
2020-11-14 23:06:04 -0800

    generate-webpage

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+// console.log(profileDataArgs);
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//       console.log(profileDataArr[i]);
+//     }
   
-    console.log('================');
-    // Is the same as this...
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
-  });
-};
+//     console.log('================');
+//     // Is the same as this...
+//   profileDataArr.forEach((profileItem) => {
+//     console.log(profileItem)
+//   });
+// };
+//const generatePage = () => 'Name: Jane, Github: janehub';
+//const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+
+// console.log(name, github);
+// console.log(generatePage(name, github));
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
