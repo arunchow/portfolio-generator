23dcba7fa5b2f0726690254a123a17774e71d295
Chowdhury <archowdhury@deloitte.com>
2020-11-15 22:37:45 -0800

    finished-site

--- app.js
+++ app.js
@@ -153,10 +153,17 @@ const inquirer = require('inquirer');
     const pageHTML = generatePage(portfolioData);
     // const pageHTML = generatePage();
 
-    fs.writeFile('./index.html', pageHTML, err => {
+    fs.writeFile('./dist/index.html', pageHTML, err => {
       if (err) throw new Error(err);
 
       console.log('Page created! Check out index.html in this directory to see it!');
+      fs.copyFile('./src/style.css', './dist/style.css', err => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log('Style sheet copied successfully!');
+      });
     });
   });
 // const fs = require('fs');
